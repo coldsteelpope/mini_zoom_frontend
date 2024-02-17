0fab71a3e70b95a7bed36bd5afd0a719df41f1cf
coldsteelpope <coldsteelpope@gmail.com>
2024-02-17 14:09:20 +0900

    change URL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
-const URL = "http://localhost:5000";
+//const URL = "http://localhost:5000";
+const URL = "https://mini-zoom-example.onrender.com";
 export const socket = io(URL);
 
